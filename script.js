5648efad3c72d8e5241c14389f93e114dae785e3
leisosag <leisosa93@gmail.com>
2021-02-17 16:15:21 -0300

    actualiza los botones play y pause y duracion del video

--- script.js
+++ script.js
@@ -1,9 +1,56 @@
+const video = document.querySelector('video');
+const progressRange = document.querySelector('.progress-range');
+const progressBar = document.querySelector('.progress-bar');
+const playBtn = document.getElementById('play-btn');
+const volumeIcon = document.getElementById('volume-icon');
+const volumeRange = document.querySelector('.volume-range');
+const volumeBar = document.querySelector('.volume-bar');
+const currentTime = document.querySelector('.time-elapsed');
+const duration = document.querySelector('.time-duration');
+const fullscreenBtn = document.querySelector('.fullscreen');
+
 // Play & Pause
+const showPlayIcon = () => {
+  playBtn.classList.replace('fa-pause', 'fa-play');
+  playBtn.setAttribute('title', 'play');
+};
+
+const togglePlay = () => {
+  if (video.paused) {
+    video.play();
+    playBtn.classList.replace('fa-play', 'fa-pause');
+    playBtn.setAttribute('title', 'pause');
+  } else {
+    video.pause();
+    showPlayIcon();
+  }
+};
+
+// on video end, show play btn icon
+video.addEventListener('ended', showPlayIcon);
 
 // Progress Bar
+// calculate display time format
+const displayTime = (time) => {
+  const minutes = Math.floor(time / 60);
+  let seconds = Math.floor(time % 60);
+  seconds = seconds > 9 ? seconds : `0${seconds}`;
+  return `${minutes}:${seconds}`;
+};
 
+const updateProgress = () => {
+  progressBar.style.width = `${(video.currentTime / video.duration) * 100}%`;
+  currentTime.textContent = `${displayTime(video.currentTime)} /`;
+  duration.textContent = `${displayTime(video.duration)}`;
+};
 // Volume Controls
 
 // Change Playback Speed
 
 // Fullscreen
+
+// event listeners
+playBtn.addEventListener('click', togglePlay);
+video.addEventListener('click', togglePlay);
+video.addEventListener('timeupdate', updateProgress);
+video.addEventListener('canplay', updateProgress);
